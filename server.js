7e9d977117d82bd08bc05a8c56c876a098a787c0
emmerz6949 <emmerz6949@gmail.com>
2020-08-06 16:39:52 -0400

    added html routes

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
