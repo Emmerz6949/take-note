f3d6feb6002f2c57fe20f13e6053513f931534b0
emmerz6949 <emmerz6949@gmail.com>
2020-08-07 23:41:45 -0400

    added api routes for get and post commands, removed the assets folder and moved its code inside the respectful tags in notes.html, since index.html wasn't using any of it I removed the lines of code that linked it and didn't bother adding said code to it, and also added a way for every note to have an id

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const path = require("path");
+const notesdb = require("./db/db.json");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/api/notes", function(req, res) {
+    res.json(notesdb);
+});
+
+app.post("/api/notes", function(req, res) {
+    const newNote = req.body;
+    notesdb.push(newNote);
+    console.log(notesdb);
+    res.json(newNote);
+});
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
